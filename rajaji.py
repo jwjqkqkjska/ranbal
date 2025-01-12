1145bf5ed94574f85e59bf4f6443f9275b6fd752
jwjqkqkjska <pulkittrial@gmail.com>
2025-01-12 11:27:23 +0530

    Update rajaji.py

--- rajaji.py
+++ rajaji.py
@@ -2,13 +2,13 @@
 #SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7747598023:AAGfi-m_9gw2L_4McP6z62OjGlbc-AavNVY")
+BOT_TOKEN=("7849069672:AAEUBwOrtw5--VPMUrX24WzoZ06iHaV9gw4")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("BGMIDDOS1245")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("1724246173")
+ADMIN_IDS=("7279164468")
 
 
 
